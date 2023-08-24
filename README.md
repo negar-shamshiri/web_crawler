71b261fb046f8c0285f6cc56a2a32592c52c41dd
negar-shamshiri <negarshamshiri@yahoo.com>
2023-08-24 11:08:11 +0430

    upload files.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # web_crawler
 Web Crawling with BeautifulSoup and Urllib
+
+## Description
+In this Python program we want to extract the table named 'جدول لیگ برتر ایران' from https://www.varzesh3.com website by extracting the tags from the HTML of it.
+
+## How to Run
+First, in main repo directory, run the following code to add `src` to your `PYTHONPATH`:
+```
+export PYTHONPATH=${PWD}
+```
+
+Then run:
+```
+python src/crawler.py
+```
---
+++ src/crawler.py
@@ -0,0 +1,24 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        response = urllib.request.urlopen(self.site)
+        html = response.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser, from_encoding='UTF-8')
+        html = sp.select('.league-standing')[0]
+        
+        with open('output/page.html', 'w') as f:
+            f.write(str(html))
+        
+        print('html file is ready!')
+
+
+if __name__ == '__main__':
+    url = "https://www.varzesh3.com/"
+    Scraper(url).scrape()
